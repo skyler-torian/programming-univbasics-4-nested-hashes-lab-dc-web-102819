a86f4f645a061813bbe63701227952715ae0fded
Skyler Torian <skymanat7@yahoo.com>
2019-10-20 22:35:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -105,7 +105,8 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     alans_new_info = ["GUI"]
+     
+     alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = alans_new_info
      programmer_hash
      
